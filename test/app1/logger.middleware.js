9f409dfd9c07adbc9d6d5fb30c9d7dc1309ff7ae
tom-n-terra <tom@terra.security>
2025-11-20 17:41:42 +0200

    Update logger.middleware.js

--- test/app1/logger.middleware.js
+++ test/app1/logger.middleware.js
@@ -1,6 +1,7 @@
 
 function simpleLogger(req, res, next) {
   const now = new Date().toISOString();
+  // this is a test
   console.log(`[${now}] ${req.method} ${req.originalUrl}`);
 
   // Continue to the next middleware/route handler
